d279b2184abfb2a3bea68354133bc71407528ef4
Wilmir Nicanor <a00278899@student.ait.ie>
2021-01-15 10:51:28 +0000

    Unit Test: Withdrawals in Savings Account

--- banktellerconsole/src/test/java/com/bankteller/entities/SavingsAccountTest.java
+++ banktellerconsole/src/test/java/com/bankteller/entities/SavingsAccountTest.java
@@ -9,17 +9,17 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 import com.bankteller.exceptions.NotEnoughBalanceException;
 import com.bankteller.exceptions.WithrawalLimitExceededException;
 
-class CurrentAccountTest {
+class SavingsAccountTest {
 
-	private Account account = new CurrentAccount();
+	private Account account;
 	
 	@BeforeEach
 	void setUp() {
-		account = new CurrentAccount();
+		account = new SavingsAccount();
 	}
 	
-	@ParameterizedTest(name = "Same day withdrawals of {1}, and {2} do not exceed the daily limit of 10000.00 and is valid for a balance of {0}")
-	@CsvFileSource(resources = "/withdrawal/current/validWithdrawalAmount.csv", numLinesToSkip = 1)
+	@ParameterizedTest(name = "Same day withdrawals of {1}, and {2} is valid for a balance of {0}")
+	@CsvFileSource(resources = "/withdrawal/savings/validWithdrawalAmount.csv", numLinesToSkip = 1)
 	void testSuccessfulWithdrawal(final double balance, final double firstWithdrawalAmount, final double secondWithdrawalAmount, 
 			final double expectedRemainingBalance, final int transactionCount) throws WithrawalLimitExceededException, NotEnoughBalanceException {
 		
@@ -37,7 +37,7 @@ class CurrentAccountTest {
 	}
 	
 	@ParameterizedTest(name = "Same day withdrawals of {1}, and {2} for a current balance of {0} is invalid")
-	@CsvFileSource(resources = "/withdrawal/current/invalidWithdrawalAmountZeroBalance.csv", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/withdrawal/savings/invalidWithdrawalAmountZeroBalance.csv", numLinesToSkip = 1)
 	void testUnSuccessfulWithdrawalDueToZeroBalance(final double balance, final double firstWithdrawalAmount, final double secondWithdrawalAmount, 
 			final double expectedRemainingBalance, final int transactionCount) throws WithrawalLimitExceededException, NotEnoughBalanceException {
 		
@@ -51,24 +51,6 @@ class CurrentAccountTest {
 
 		assertEquals(firstWithdrawalAmount, transaction1.getAmount());
 		assertEquals("Account does not have enough balance", exception.getMessage());
-
-	}
-	
-	@ParameterizedTest(name = "Same day withdrawals of {1}, and {2} exceeds daily limit of 10000.00")
-	@CsvFileSource(resources = "/withdrawal/current/invalidWithdrawalAmountDailyLimit.csv", numLinesToSkip = 1)
-	void testUnSuccessfulWithdrawalDueToDailyLimit(final double balance, final double firstWithdrawalAmount, final double secondWithdrawalAmount, 
-			final double expectedRemainingBalance, final int transactionCount) throws WithrawalLimitExceededException, NotEnoughBalanceException {
-		
-		account.setBalance(balance);
-		
-		final Transaction transaction1 = account.withdraw(firstWithdrawalAmount);
-		final Throwable exception = assertThrows(WithrawalLimitExceededException.class, () -> account.withdraw(secondWithdrawalAmount));
-		
-		assertEquals(transactionCount, account.getTransactions().size());
-		assertEquals(expectedRemainingBalance, account.getBalance(), 0.001);
-
-		assertEquals(firstWithdrawalAmount, transaction1.getAmount());
-		assertEquals("Withdrawal limit is reached", exception.getMessage());
 
 	}
 
